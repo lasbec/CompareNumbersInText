28b65751cf131717aa2d34ad0dafa3d2b5519be1
Lasse Becker <lasse.becker@idalabs.de>
2023-07-15 10:34:41 +0200

    first commit

---
+++ CompareNumbersInText.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+    
+
+def filterDigits(str):
+    return "".join([c for c in str if c.isdigit()])
+
+def firstDiff(str0, str1):
+    result = ""
+    for c0, c1 in zip(str0, str1):
+        if(c0!=c1):
+            return result + " (common prefix)" + "\n  " + c0 + " first difference\n  " + c1 + " first difference"
+        result += c0
+
+    return None
+
+
+class StringSlice:
+    def __init__(self, parentString, startIndex, stopIndex):
+        self.parentString = parentString
+        self.parentLen = len(parentString)
+        self.startIndex = startIndex
+        self.stopIndex = stopIndex
+        self.value = parentString[startIndex:stopIndex]
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __repr__(self):
+        return f"[{self.startIndex}:{self.stopIndex}]'{self.value}'"
+
+    def hintString(self):
+        return self.surroundingSlice().valueWithOpenEndsMarked()
+
+    def valueWithOpenEndsMarked(self):
+        leftEndIsOpen = self.startIndex > 0
+        rightEndIsOpen = self.stopIndex < self.parentLen
+        leftMark = "..." if leftEndIsOpen else ""
+        rightMark = "..." if rightEndIsOpen else ""
+        return leftMark + self.value + rightMark
+
+    def surroundingSlice(self,radius=10):
+        start = self.startIndex - radius
+        start = 0 if start < 0 else start
+
+        stop = self.stopIndex + radius
+        stop = self.parentLen if stop > self.parentLen else stop
+
+        return StringSlice(self.parentString, start, stop)
+
+class DigitsComparison:
+    def __init__(self, slicesLeft, slicesRight):
+        self.slicesLeft = slicesLeft
+        self.slicesRight = slicesRight
+        
+    def differences(self):
+        result = []
+        for leftDigits, rightDigits in zip(self.sliceLeft, self.sliceRight):
+            if(leftDigist != rightDigits):
+                result += [(leftDigits, rightDigits)]
+        return result
+
+
+    def __repr__(self):
+        return self.differences().__repr__()
+
+        
+
+
+def parseDigitSlices(string):
+    result = []
+    startOfDigits = None
+    for index, char in enumerate(string):
+        if (char.isdigit() and startOfDigits == None):
+           startOfDigits = index
+        elif (not char.isdigit() and startOfDigits != None):
+           result.append(StringSlice(string, startOfDigits, index))
+           startOfDigits = None
+    if(startOfDigits != None): # case last char is a digit   
+        result.append(StringSlice(string, startOfDigits, len(string)))
+    return result
+
+
+
+
+
+def main():
+    counter = 0
+    df = pd.read_excel('Texte.xlsx')
+    col1 = df["DE"].tolist()
+    col2 = df["FR"].tolist()
+    for i,(cell1, cell2) in enumerate(zip(col1, col2)):
+        _numbers1 = filterDigits(cell1)
+        _numbers2 = filterDigits(cell2)
+
+        numbers1 = parseDigitSlices(cell1)
+        numbers2 = parseDigitSlices(cell2)
+        
+        DigitsComparison
+
+        firstNumber = _numbers[0]
+        if any(n != firstNumber for n in _numbers):
+            fstEntry = pair[0]
+            counter += 1
+            print("\nEntry unequal in line", i + 2, "\nRead numbers\n", _numbers[0], "\n", _numbers[1],"\n", firstDiff(_numbers[0], _numbers[1])),
+
+    print("Found ", counter, " inconsistencys in total")
+
+main()
